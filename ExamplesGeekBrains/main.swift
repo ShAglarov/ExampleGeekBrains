bf5c7b5cb08cb3e45a734c829a40e5779e940b7b
Aglarov <shamil.aglarov@gmail.com>
2022-06-24 01:25:10 +0300

    Closures_Example-03

--- ExamplesGeekBrains/main.swift
+++ ExamplesGeekBrains/main.swift
@@ -1,16 +1,21 @@
 //
 //  Developer -> main.swift
 //  Created by Shamil Aglarov on 24.06.2022.
-//  Theme Closures: Example - 02
+//  Theme Closures: Example - 03
 //
 
 import Foundation
 
-func sendRequest(complition: (Bool) -> Void) {
-    let isSuccess = Bool.random()
-    complition(isSuccess)
-}
+var array = [1, 2, 3, 4]
 
-sendRequest(complition: {
-    print($0)
+let newArray = array.filter( {
+    return $0 % 2 == 0
 })
+
+print(newArray)
+
+let sortArray = array.sorted(by: {
+    return $0 < $1
+})
+
+print(sortArray)
