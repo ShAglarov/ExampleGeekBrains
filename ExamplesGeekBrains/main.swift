520f09cb582810cb9000af398658b047553f8d21
Aglarov <shamil.aglarov@gmail.com>
2022-06-24 01:03:44 +0300

    Generic_Example-01

--- ExamplesGeekBrains/main.swift
+++ ExamplesGeekBrains/main.swift
@@ -1,11 +1,26 @@
 //
-//  main.swift
-//  ExamplesGeekBrains
-//
+//  Developer -> main.swift
 //  Created by Shamil Aglarov on 24.06.2022.
+//  Theme Generics: Example - 01
 //
-
 import Foundation
 
-print("Hello, World!")
+struct Storage<T> {
+    var elements: [String: T] = [:]
+    
+    mutating func setElement(_ element: T, for key: String) {
+        elements[key] = element
+    }
+    
+    mutating func getElement(for key: String) -> T? {
+        return elements[key]
+    }
+}
 
+var storage = Storage<Int>()
+
+storage.setElement(33, for: "Shamil")
+storage.setElement(30, for: "Kurban")
+
+print(storage.getElement(for: "Shamil"))
+print(storage.getElement(for: "Kurban"))
