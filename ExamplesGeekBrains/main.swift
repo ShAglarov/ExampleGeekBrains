8c7b0a1bf4551035f122a35abf5e25a84b880707
Aglarov <shamil.aglarov@gmail.com>
2022-06-24 01:16:29 +0300

    Subscript_Example-02

--- ExamplesGeekBrains/main.swift
+++ ExamplesGeekBrains/main.swift
@@ -1,46 +1,20 @@
 //
 //  Developer -> main.swift
 //  Created by Shamil Aglarov on 24.06.2022.
-//  Theme Subscript: Example - 01
+//  Theme Subscript: Example - 02
 //
 import Foundation
 
-protocol StorageIdentifiable {
-    var id: String { get }
-}
-
-struct Storage<T: StorageIdentifiable> {
-    var elements: [String: T] = [:]
-    
-    mutating func setElement(_ element: T) {
-        elements[element.id] = element
-    }
-    
-    mutating func getElement(by id: String) -> T? {
-        return elements[id]
-    }
-    
-    subscript(id: String) -> T? {
-        get {
-            return  elements[id]
+extension Array {
+    subscript(safe index: Int) -> Element? {
+        if index < 0 || index > count - 1 {
+            return nil
         }
-        set {
-            elements[id] = newValue
-        }
+        return self[index]
     }
 }
 
-struct Book: StorageIdentifiable {
-    let id: String
-    let title: String
-}
+let elements = [1,2,3,4]
 
-let book = Book(id: "1", title: "Книга Усова Swift")
-var storage = Storage<Book>()
-
-storage["1"] = book
-
-let someObj = storage["1"]
-
-print(someObj)
-
+print(elements[safe: 3]!)
+print(elements[safe: 4])
