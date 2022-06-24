a275c3ca65be8e7a8b3035d7c5991ece8f20fdb4
Aglarov <shamil.aglarov@gmail.com>
2022-06-24 20:14:28 +0300

    TryCatch_Example-01

--- ExamplesGeekBrains/main.swift
+++ ExamplesGeekBrains/main.swift
@@ -1,11 +1,42 @@
 //
-//  main.swift
-//  ExamplesGeekBrains
-//
+//  Developer -> main.swift
 //  Created by Shamil Aglarov on 24.06.2022.
+//  Theme TryCatch: Example - 01
 //
-
 import Foundation
 
-print("Hello, World!")
+enum UserDataError: Error {
+    case noName
+    case inCorrectAge
+    case ageLessThenZero
+}
+
+func enterName() throws -> String {
+    print("Ввведите имя")
+    let name = readLine()
+    
+    guard let name = name, !name.isEmpty else { throw UserDataError.noName }
+    
+    return name
+}
+
+func enterAge() throws -> Int {
+    print("Введите возраст")
+    let ageString = readLine()
+    
+    guard let ageString = ageString, let age = Int(ageString) else {
+        throw UserDataError.ageLessThenZero
+    }
+    return age
+}
+
+do {
+    _ = try enterName()
+    _ = try enterAge()
+    
+    print("Данные корректны")
+} catch(let dataError) {
+    print("Данные не корректны \(dataError)")
+}
+
 
