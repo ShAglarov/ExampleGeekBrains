e33f166937a8609b78d0ed583647fae537c7135c
Aglarov <shamil.aglarov@gmail.com>
2022-06-26 03:59:07 +0300

    Delegate_CookOrder_Example-02

--- ExamplesGeekBrains/main.swift
+++ ExamplesGeekBrains/main.swift
@@ -1,12 +1,35 @@
 //
-//  main.swift
-//  ExamplesGeekBrains
-//
+//  Developer -> main.swift
 //  Created by Shamil Aglarov on 24.06.2022.
+//  Theme Delegate: Example - 02
 //
 
 import Foundation
 
-let lifeBunny = LifeOfBunny()
+protocol ElevatorDelegate {
+    func cookFood(_ order: String) -> Bool
+}
 
-lifeBunny.startLifeBunny()
+class Waiter {
+    
+    func getOrder() -> String {
+        print("Что вы будете сэр")
+        return readLine() ?? ""
+    }
+    
+    var delegate: ElevatorDelegate = cook
+}
+
+class Cook: ElevatorDelegate {
+    
+    func cookFood(_ order: String) -> Bool {
+        print("Я приготовил \(order)")
+        return true
+    }
+}
+
+let cook = Cook()
+
+let waiter = Waiter()
+
+waiter.delegate.cookFood(waiter.getOrder())
